2fd434152df141ed5fb1b8bcfb0193babf086fb1
Daniel Sobczak <20947896+TheLaw1337@users.noreply.github.com>
2020-07-01 21:06:17 +0200

    Task 2 Modern

---
+++ cypress/integration/ModernTestsV1/task2-visuals.spec.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+import Page from './page_objects/page'
+
+describe('Task 1', () => {
+    beforeEach(() => {
+        cy.viewport(800, 600);
+        cy.eyesOpen({
+          appName: 'Hackathon V1',
+          testName: 'Task 2',
+          browser: [
+            {width: 1200, height: 700, name: 'chrome'},
+            {width: 1200, height: 700, name: 'firefox'},
+            {width: 1200, height: 700, name: 'edgechromium'},
+            {width: 768, height: 700, name: 'chrome'},
+            {width: 768, height: 700, name: 'firefox'},
+            {width: 768, height: 700, name: 'edgechromium'},
+            {deviceName: 'iPhone X', screenOrientation: 'portrait'},
+          ]
+        });
+      });
+    
+    afterEach(() => {
+        cy.eyesClose();
+    });
+
+    it('should load a page and take screenshot', () => {
+        const page = new Page();
+
+        page.navigateV1();
+        page.filterBlackShoes();
+        cy.eyesCheckWindow({
+            tag: 'Filter Results',
+            target: 'region',
+            selector: {
+                type: 'css',
+                selector: '#product_grid'
+            }   
+        });
+    })
+})
--- cypress/integration/page_objects/page.js
+++ cypress/integration/page_objects/page.js
@@ -5,6 +5,11 @@ class Page {
     navigateV2() {
         cy.visit('https://demo.applitools.com/gridHackathonV2.html');
     }
+    filterBlackShoes() {
+        cy.get('#ti-filter').click();
+        cy.get('#colors__Black').check();
+        cy.get('#filterBtn').click();
+    }
 }
 
 export default Page;
